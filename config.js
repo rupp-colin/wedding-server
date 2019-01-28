ad5c1ab030e2c91218f51e12a8ef0733724f1749
Colin Rupp <colin@thinkful.com>
2019-01-28 13:25:21 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  DATABASE_URL:
+  process.env.DATABASE_URL || 'mongodb://localhost:27017',
+  TEST_DATABASE_URL:
+  process.env.TEST_DATABASE_URL ||
+  '',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.envJWT_EXPIRY || '4d'
+
+};
+
